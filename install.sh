e69207d2af5b371dbbfbbe947d2c840a1cd9556a
rushabhp <rushabhpatil@tutamail.com>
2025-03-08 20:15:24 +0530

    updated to a more nicer install.sh file - just learning :)

--- install.sh
+++ install.sh
@@ -1,9 +1,30 @@
 #!/bin/sh
 
-sudo pacman -Syu
-sudo pacman -S ttf-jetbrains-mono-nerd $1 alacritty rofi-wayland $2
+set -e
 
+# Function to install required packages
+install_packages() {
+  sudo pacman -Syu
+  sudo pacman -S ttf-jetbrains-mono-nerd alacritty rofi-wayland $1 $2
+}
 
-# BACK UP existing config files :)
-rm -rf ~/.config/{hypr,rofi,alacritty}
-cp -r config/* ~/.config/
+# Function to back up and copy configuration files
+backup_and_copy_configs() {
+  rm -rf ~/.config/{hypr,rofi,alacritty}
+  cp -r config/* ~/.config/
+}
+
+# Validate input parameters
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <package1> <package2>"
+  exit 1
+fi
+
+# Main function
+main() {
+  install_packages "$@"
+  backup_and_copy_configs
+}
+
+# Execute main function with all input parameters
+main "$@"
