b57b41977c1c9b63c1ec7d84df71490ac4597405
rushabhp <rushabhpatil@tutamail.com>
2025-03-08 20:51:15 +0530

    install-sddm-theme

--- install.sh
+++ install.sh
@@ -4,8 +4,7 @@ set -e
 
 # Function to install required packages
 install_packages() {
-  sudo pacman -Syu
-  yay -S ttf-jetbrains-mono-nerd alacritty rofi-wayland swaync hyprpaper-git hyprland-git pavucontrol sof-firmware pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-rtkit wireplumber sddm wl-clipboard
+  yay -Syu ttf-jetbrains-mono-nerd alacritty rofi-wayland swaync hyprpaper-git hyprland-git pavucontrol sof-firmware pipewire pipewire-alsa pipewire-jack pipewire-pulse pipewire-rtkit wireplumber sddm wl-clipboard
 }
 
 # Function to back up and copy configuration files
@@ -13,11 +12,18 @@ delete_and_copy_configs() {
   rm -rf ~/.config/{hypr,rofi,alacritty}
   cp -r config/* ~/.config/
 }
+
+install_sddm_theme() {
+    sh -c "$(curl -fsSL https://raw.githubusercontent.com/keyitdev/sddm-astronaut-theme/master/setup.sh)"
+}
+
 # Main function
 main() {
   install_packages "$@"
   delete_and_copy_configs
+  install_sddm_theme
 }
+
 
 # Execute main function with all input parameters
 main "$@"
