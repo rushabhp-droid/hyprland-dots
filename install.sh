d2c35c2b7ca131966f78ae9622ca8190ef9382b0
rushabhp <rushabhpatil@tutamail.com>
2025-03-08 20:20:48 +0530

    deleted unnecessary things

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 # Function to install required packages
 install_packages() {
   sudo pacman -Syu
-  sudo pacman -S ttf-jetbrains-mono-nerd alacritty rofi-wayland $1 $2
+  yay -S ttf-jetbrains-mono-nerd alacritty rofi-wayland swaync hyprpaper-git hyprland-git
 }
 
 # Function to back up and copy configuration files
@@ -13,13 +13,6 @@ delete_and_copy_configs() {
   rm -rf ~/.config/{hypr,rofi,alacritty}
   cp -r config/* ~/.config/
 }
-
-# Validate input parameters
-if [ -z "$1" ] || [ -z "$2" ]; then
-  echo "Usage: $0 <package1> <package2>"
-  exit 1
-fi
-
 # Main function
 main() {
   install_packages "$@"
